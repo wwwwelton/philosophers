07c51615302410df2330cbcae54a8f8c6d17b9ef
wleite <wleite@student.42sp.org.br>
2021-12-29 03:21:12 -0300

    chore: removed comments

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,13 +6,11 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 22:46:42 by wleite            #+#    #+#             */
-/*   Updated: 2021/12/29 03:00:24 by wleite           ###   ########.fr       */
+/*   Updated: 2021/12/29 03:20:50 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-//n_of_philos || time_to_die || time_to_eat || time_to_sleep || times_must_eat
 
 int	main(int argc, char **argv)
 {
