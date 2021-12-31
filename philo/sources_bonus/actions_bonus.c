bd844293ddfd7c9b3719c5793527f3dc5012b3da
wleite <wleite@student.42sp.org.br>
2021-12-31 14:09:45 -0300

    refactor: unlock print_action semaphore only if philo is not died

--- philo/sources_bonus/actions_bonus.c
+++ philo/sources_bonus/actions_bonus.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 07:56:08 by wleite            #+#    #+#             */
-/*   Updated: 2021/12/31 06:09:54 by wleite           ###   ########.fr       */
+/*   Updated: 2021/12/31 14:07:38 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@ static int	go_eat_alone(t_philo *philo)
 	sem_wait(philo->fork_right);
 	print_action(philo, TOOK_A_FORK);
 	msleep(philo->data->time_to_die);
-	print_dead_action(philo);
+	print_action(philo, DIED);
 	sem_post(philo->fork_right);
 	return (1);
 }
 
 static void	go_eat(t_philo *philo)
 {
+	if (dmsleep_of_death(philo))
+	{
+		print_action(philo, DIED);
+		exit_philo(philo->data, philo->forks, philo->philos, 1);
+	}
 	sem_wait(philo->fork_right);
 	sem_wait(philo->fork_left);
 	print_action(philo, TOOK_A_FORK);
@@ -32,7 +37,7 @@ static void	go_eat(t_philo *philo)
 	philo->lastsupper = timenow(philo->data->firststamp);
 	if (msleep_of_death(philo->data->time_to_eat, philo))
 	{
-		print_dead_action(philo);
+		print_action(philo, DIED);
 		exit_philo(philo->data, philo->forks, philo->philos, 1);
 	}
 	sem_post(philo->fork_right);
@@ -45,7 +50,7 @@ static void	go_sleep(t_philo *philo)
 	print_action(philo, SLEEPING);
 	if (msleep_of_death(philo->data->time_to_sleep, philo))
 	{
-		print_dead_action(philo);
+		print_action(philo, DIED);
 		exit_philo(philo->data, philo->forks, philo->philos, 1);
 	}
 }
