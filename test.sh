e00901b537b8b1a624311c7f8eddc308f1565c99
wleite <wleite@student.42sp.org.br>
2022-01-01 00:19:21 -0300

    test: changed number of tests dynamically

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# make bonus && clear
-# exe=./philo_bonus
-make && clear
-exe=./philo
+# make -C philo_bonus && clear
+# exe=./philo_bonus/philo_bonus
+make -C philo && clear
+exe=./philo/philo
+
+tests=1
+
 x=1
 time=0.1
 echo -e "\e[41m 1 400 100 100 7 should die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;31mTest: $x\e[0m"
 	$exe 1 400 100 100 7 | grep die
@@ -19,7 +22,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[42m 4 210 100 100 7 not die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;32mTest: $x\e[0m"
 	$exe 4 210 100 100 7 | grep die
@@ -32,7 +35,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[41m 1 800 200 200 7 should die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;31mTest: $x\e[0m"
 	$exe 1 800 200 200 7 | grep die
@@ -45,7 +48,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[42m 5 800 200 200 7 not die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;32mTest: $x\e[0m"
 	$exe 5 800 200 200 7 | grep die
@@ -58,7 +61,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[41m 4 310 200 100 7 should die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;31mTest: $x\e[0m"
 	$exe 4 310 200 100 7 | grep die
@@ -71,7 +74,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[42m 5 400 100 100 7 not die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;32mTest: $x\e[0m"
 	$exe 5 400 100 100 7 | grep die
@@ -84,7 +87,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[41m 5 200 100 100 7 should die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;31mTest: $x\e[0m"
 	$exe 5 200 100 100 7 | grep die
@@ -97,7 +100,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[42m 3 400 100 100 7 not die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;32mTest: $x\e[0m"
 	$exe 3 400 100 100 7 | grep die
@@ -110,7 +113,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[41m 3 200 100 100 7 should die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;31mTest: $x\e[0m"
 	$exe 3 200 100 100 7 | grep die
@@ -123,7 +126,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[42m 4 410 200 200 7 not die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;32mTest: $x\e[0m"
 	$exe 4 410 200 200 7 | grep die
@@ -136,7 +139,7 @@ echo
 x=1
 time=0.1
 echo -e "\e[41m 4 399 200 200 7 should die\e[0m"
-while [ $x -le 10 ]
+while [ $x -le $tests ]
 do
 	echo -e "\e[1;31mTest: $x\e[0m"
 	$exe 4 399 200 200 7 | grep die
@@ -146,4 +149,5 @@ done
 
 echo
 
-make fclean
+make -C ./philo fclean
+make -C ./philo_bonus fclean
