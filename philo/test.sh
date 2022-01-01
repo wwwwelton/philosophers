0e558a997e46436e65d15436760672ba3f1c9019
wleite <wleite@student.42sp.org.br>
2021-12-31 22:43:20 -0300

    test: added test script

---
+++ philo/test.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+# make bonus && clear
+# exe=./philo_bonus
+make && clear
+exe=./philo
+x=1
+time=0.1
+echo -e "\e[41m 1 400 100 100 7 should die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;31mTest: $x\e[0m"
+	$exe 1 400 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[42m 4 210 100 100 7 not die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;32mTest: $x\e[0m"
+	$exe 4 210 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[41m 1 800 200 200 7 should die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;31mTest: $x\e[0m"
+	$exe 1 800 200 200 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[42m 5 800 200 200 7 not die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;32mTest: $x\e[0m"
+	$exe 5 800 200 200 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[41m 4 310 200 100 7 should die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;31mTest: $x\e[0m"
+	$exe 4 310 200 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[42m 5 400 100 100 7 not die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;32mTest: $x\e[0m"
+	$exe 5 400 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[41m 5 200 100 100 7 should die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;31mTest: $x\e[0m"
+	$exe 5 200 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[42m 3 400 100 100 7 not die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;32mTest: $x\e[0m"
+	$exe 3 400 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[41m 3 200 100 100 7 should die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;31mTest: $x\e[0m"
+	$exe 3 200 100 100 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[42m 4 410 200 200 7 not die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;32mTest: $x\e[0m"
+	$exe 4 410 200 200 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+x=1
+time=0.1
+echo -e "\e[41m 4 399 200 200 7 should die\e[0m"
+while [ $x -le 10 ]
+do
+	echo -e "\e[1;31mTest: $x\e[0m"
+	$exe 4 399 200 200 7 | grep die
+	sleep $time
+	x=$(( $x + 1 ))
+done
+
+echo
+
+make fclean
